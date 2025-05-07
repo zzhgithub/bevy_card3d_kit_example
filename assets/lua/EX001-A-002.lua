68a415e09d5a12d0b81c40aa04742e0f3acef3b8
zhouzihao <1042181618@qq.com>
2025-05-07 19:18:59 +0800

    dev-使用self支持的脚本

--- assets/lua/EX001-A-002.lua
+++ assets/lua/EX001-A-002.lua
@@ -1,8 +1,10 @@
 local ns = "entity_" .. entity.index;
 
 _G[ns] = {
+    id = "EX001-A-002",
+    name = "门"
 };
 
-_G[ns].get_card_info = function()
-    return CardInfo("EX001-A-002", "门");
+_G[ns].get_card_info = function(self)
+    return CardInfo(self.id, self.name);
 end
--- assets/lua/NAAI-A-001.lua
+++ assets/lua/NAAI-A-001.lua
@@ -1,8 +1,10 @@
 local ns = "entity_" .. entity.index;
 
 _G[ns] = {
+    id = "NAAI-A-001",
+    name = "维尔汀"
 };
 
-_G[ns].get_card_info = function()
-    return CardInfo("NAAI-A-001", "维尔汀");
+_G[ns].get_card_info = function(self)
+    return CardInfo(self.id, self.name);
 end
